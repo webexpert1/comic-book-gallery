d89ce39ae9d010a2c9977f14514a2723c25eb2c4
Emmanuel Eze <emmanuel.eze@netopng.com>
2019-08-24 21:38:31 +0100

    add comicbooks controller

---
+++ comic-book-gallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace comic_book_gallery.Controllers
+{
+    public class ComicBooksController: Controller
+    {
+        public ActionResult Detail()
+        {
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from comic books controller");
+           
+            
+        }
+    }
+}
